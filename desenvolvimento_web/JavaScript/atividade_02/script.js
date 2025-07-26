c615a5609cfb8e5a6a6cca327fd47e47b4d700bf
Hugo Pereira <hugoper_cs@yahoo.com>
2025-07-26 17:58:38 -0300

    adiciona exemplos e atividades da disciplina desenvolvimento web

---
+++ desenvolvimento_web/JavaScript/atividade_02/script.js
@@ -0,0 +1,17 @@
+function somar(a, b) {
+    return a + b;
+}
+function subtrair(a, b) {
+    return a - b;
+}
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b == 0) alert('Divisão por zero não é permitida.');
+    return a / b;
+}
+function exponencial(a, b) {
+    return Math.pow(a, b);
+}
